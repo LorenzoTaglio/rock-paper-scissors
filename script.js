018a2d612a2f553999707a2c598b8f5fb45248fc
Lorenzo Taglio <lorenzo.taglio.work@gmail.com>
2023-05-03 16:36:38 +0200

    Create game loop
    Make sure that the user asks for valid input. Use comma inside
    ternary operator to inform the user that input is invalid.
    Also clean and make small corrections to the code.

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ ELSE
     RETURN Scissors
 */
 function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random()*3);
-    if(randomChoice === 0)
-        return "Rock";
-    else if(randomChoice === 1)
-        return "Paper";
-    else
-        return "Scissors";
+  let randomChoice = Math.floor(Math.random() * 3);
+  if (randomChoice === 0) return "Rock";
+  else if (randomChoice === 1) return "Paper";
+  else return "Scissors";
 }
 
-function capitalize(string){
-    return string.substring(0,1).toUpperCase() + string.substring(1).toLowerCase();
+function capitalize(string) {
+  if (string !== null)
+    return (
+      string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase()
+    );
 }
 
 /*
@@ -39,33 +39,76 @@ CREATE variable win and set it to null
         ELSE IF computer is Rock
             SET win to false
 */
-function playRound(playerSelection, computerSelection){
-    let win;
-    playerSelection = capitalize(playerSelection);
-    switch (playerSelection) {
-        case "Rock":
-            win = computerSelection === "Scissors" ? true
-                : computerSelection ==="Paper" ? false 
-                : null;
-            break;
-        case "Paper":
-            win = computerSelection === "Rock" ? true
-                : computerSelection === "Scissors" ? false
-                : null;
-            break;
-        case "Scissors":
-            win = computerSelection === "Paper" ? true
-                : computerSelection === "Rock" ? false
-                : null
-            break;
-        default:
-            return "Player selection invalid.";
-    }
+function playRound(playerSelection, computerSelection) {
+  let win;
+  switch (playerSelection) {
+    case "Rock":
+      win =
+        computerSelection === "Scissors"
+          ? true
+          : computerSelection === "Paper"
+          ? false
+          : null;
+      break;
+    case "Paper":
+      win =
+        computerSelection === "Rock"
+          ? true
+          : computerSelection === "Scissors"
+          ? false
+          : null;
+      break;
+    case "Scissors":
+      win =
+        computerSelection === "Paper"
+          ? true
+          : computerSelection === "Rock"
+          ? false
+          : null;
+      break;
+  }
 
-    if(win === true)
-        return "You win! " + playerSelection + " beats " + computerSelection
-    else if(win === false)
-        return "You lose! " + computerSelection + " beats " + playerSelection
-    else if(win === null)
-        return "It's a tie!"
+  return win;
 }
+
+function game() {
+  let playerSelection, computerSelection;
+  let playerWins = 0,
+    computerWins = 0;
+  let roundResults;
+  for (let i = 0; i < 5; i++) {
+    // Asks the user for input until it is valid
+    do {
+      playerSelection = capitalize(prompt("Choose move"));
+    } while (
+      playerSelection !== "Rock" &&
+      playerSelection !== "Paper" &&
+      playerSelection !== "Scissors"
+        ? (console.log("Player selection invalid."), true)
+        : false
+    );
+
+    computerSelection = getComputerChoice();
+
+    roundResults = playRound(playerSelection, computerSelection);
+    if (roundResults === true) {
+      playerWins++;
+      console.log(
+        "You win! " + playerSelection + " beats " + computerSelection
+      );
+    } else if (roundResults === false) {
+      computerWins++;
+      console.log(
+        "You lose! " + computerSelection + " beats " + playerSelection
+      );
+    } else if (roundResults === null) {
+      console.log("It's a tie!");
+    }
+  }
+
+  if (playerWins > computerWins) console.log("Player wins!");
+  else if (computerWins > playerWins) console.log("Computer wins!");
+  else console.log("Tie!");
+}
+
+game();

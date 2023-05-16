d2ad336f0f2f3e44ff5cbc50c11fe3ecef05fb88
Lorenzo Taglio <lorenzo.taglio.work@gmail.com>
2023-05-16 12:48:59 +0200

    Make buttons return player choice
    Comment useless code, buttons need to be reworked.

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-/*
-Get a random number between 1 and 2
-IF the random number is 0
-    RETURN Rock
-ELSE IF the random number is 1
-    RETURN Paper
-ELSE
-    RETURN Scissors
-*/
+const buttons = document.querySelectorAll(".buttons > button");
+
+buttons.forEach(button =>{
+  button.addEventListener("click", (e)=>{console.log(getPlayerChoice(e))});
+})
+
+function getPlayerChoice(event) {
+  return event.target.dataset.move
+}
+
+
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
   if (randomChoice === 0) return "Rock";
   else if (randomChoice === 1) return "Paper";
   else return "Scissors";
 }
-
+/*(NO LONGER NEEDED)
 function capitalize(string) {
   if (string !== null)
     return (
       string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase()
     );
 }
-
-/*
-CREATE variable win and set it to null
-    IF player is Rock 
-        IF computer is Scissors
-            SET win to true
-        ELSE IF computer is Paper
-            SET win to false
-    ELSE IF player is Paper
-        IF computer is Rock
-            SET win to true
-        ELSE IF computer Scissors
-            SET win to false
-    ELSE
-        IF computer is Paper
-            SET win to true
-        ELSE IF computer is Rock
-            SET win to false
 */
 function playRound(playerSelection, computerSelection) {
   let win;
@@ -77,7 +61,8 @@ function game() {
     computerWins = 0;
   let roundResults;
   for (let i = 0; i < 5; i++) {
-    // Asks the user for input until it is valid
+    // Asks the user for input until it is valid (NO LONGER NEEDED)
+    /*
     do {
       playerSelection = capitalize(prompt("Choose move"));
     } while (
@@ -87,7 +72,7 @@ function game() {
         ? (console.log("Player selection invalid."), true)
         : false
     );
-
+    */
     computerSelection = getComputerChoice();
 
     roundResults = playRound(playerSelection, computerSelection);
@@ -111,4 +96,6 @@ function game() {
   else console.log("Tie!");
 }
 
-game();
+
+
+// game();
